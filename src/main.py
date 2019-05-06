769c8f389ec3a0d094b1ccc84fd608b8af61a6f9
Julian van Doorn <j.g.doornvan@gmail.com>
2019-05-06 15:36:44 +0200

    Added src/

---
+++ src/main.py
@@ -0,0 +1,10 @@
+# Make Simulator visibile to main.py
+import sys
+sys.path.append("../simulator-3.x")
+
+# pylint: disable=F0401
+# Suppress lint error
+import Simulator
+
+if __name__ == "__main__":
+    pass
