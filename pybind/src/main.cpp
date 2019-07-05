0cee1ede634b0c0d433af0f9377c64ce2dcd9e95
Julian van Doorn <j.g.doornvan@gmail.com>
2019-07-05 17:07:52 +0200

    Removed nonfunctioning main

--- pybind/src/main.cpp
+++ pybind/src/main.cpp
@@ -8,17 +8,7 @@
 std::unique_ptr<lemonator_proxy> limo;
 
 int main() {
-    int p = 3;
 
-    std::cout << "Opening COM" << p << std::endl;
-    limo = std::make_unique<lemonator_proxy>(p, 1, 1);
-    std::cout << "New limo proxy at: " << (void*) limo.get() << std::endl;
-
-    // auto color = limo->color.read_rgb();
-    // std::cout << "Reading rgb (" << color.r << ", " << color.g << ", " << color.b << std::endl;
-    
-    auto distance = limo->distance.read_mm();
-    std::cout << "Received distance: " << distance << std::endl;
 }
 #else
 #include "py-interface.hpp"
